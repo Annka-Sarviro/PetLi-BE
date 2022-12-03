98cfadf16d9a39d4487a5cd19e198c01c1b7f6db
Ann Sarviro <annka_nuha@ai.ua>
2022-12-03 18:22:15 +0200

    add cloudinary

--- controllers/users/updateUserAvatar.js
+++ controllers/users/updateUserAvatar.js
@@ -1,34 +1,39 @@
 const fs = require("fs/promises");
-const path = require("path");
+// const path = require("path");  // need for avatarDir
 const createError = require("http-errors");
 const Jimp = require("jimp");
+const cloudinary = require("../../helpers/cloudinary");
 
 const { User } = require("../../models/user");
 
-const avatarDir = path.join(__dirname, "../../", "public", "avatars");
+// const avatarDir = path.join(__dirname, "../../", "public", "avatars");
 const imgSizePx = 250;
 
 const updateUserAvatar = async (req, res) => {
   try {
     const { _id } = req.user;
-
-    const { path: tempUpload, originalname } = req.file;
+    const { path: tempUpload } = req.file;
 
     const jimpAvatar = await Jimp.read(tempUpload);
     await jimpAvatar.resize(imgSizePx, imgSizePx, Jimp.RESIZE_BEZIER).writeAsync(tempUpload);
 
-    const extention = originalname.split(".").pop();
-    const filename = `${_id}.${extention}`;
-    const resultUpload = path.join(avatarDir, filename);
+    const uploader = async path => await cloudinary.uploads(path, "petly_dir/avatars");
+    const newPath = await uploader(tempUpload);
+    fs.unlink(req.file.path);
 
-    await fs.rename(tempUpload, resultUpload);
-    const avatar = path.join("avatars", filename);
+    await User.findByIdAndUpdate(
+      _id,
+      { avatar: newPath.url },
+      {
+        new: true,
+      }
+    );
 
-    await User.findByIdAndUpdate(_id, { avatar });
-    res.json(avatar);
+    res.json({ message: "success", avatar: newPath.url });
   } catch (error) {
-    await fs.unlink(req.files.avatar.filepath);
-    throw createError(401, "Not authorized");
+    // await fs.unlink(req.file.path);
+
+    throw createError(400, error.message);
   }
 };
 
